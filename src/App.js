a50d9da81f3a9d7057e8ad1bcef57d1270d52667
Akshay-1201 <akshaykute12@gmail.com>
2023-06-24 20:04:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [data,setData]=useState(0)
+  function updateData()
+  {
+    setData(data+1)
+   
+  }
+  function decrement(){
+    setData(data-1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 style={{marginLeft: '50%'}}>{data}</h1>
+     <button onClick={updateData} style={{color: 'red',marginLeft: '45%'}}>Counter</button>
+     <button onClick={decrement}style={{marginLeft: '10px',color:'blue'}} >Decount</button>
     </div>
   );
 }
